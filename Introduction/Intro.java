c690318560f868bc8ca891dfcc40bb6a9299ba84
Gowtham <gowtham.vasista@gmail.com>
2015-03-15 17:45:31 +0530

    a introduction to java

---
+++ Introduction/Intro.java
@@ -0,0 +1,26 @@
+// Java introduction file
+
+class A {
+	
+	// some code
+	
+}
+
+class B {
+	
+	//some code 
+	
+}
+
+class X {
+	
+	public static void main (String[] args) {
+		//creating object for A
+		A a = new A();
+		//creating object for B
+		B b = new B();
+		
+		// some code
+		
+	}
+}
